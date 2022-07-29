dfcbb536f0348e989902f259f91281b32aad24e0
Mehul Oswal <mehuloswal21@gmail.com>
2022-07-29 16:31:49 +0530

    Initial Commit

---
+++ Async-Image/Async_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Async_ImageApp.swift
+//  Async-Image
+//
+//  Created by Mehul Oswal on 29/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Async_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
